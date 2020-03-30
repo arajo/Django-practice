ea952cfd30a4859b9275da3e0823611079565ad3
Ara jo <i_tomorrow_i@hanmail.net>
2020-03-30 14:41:00 +0900

    로그인, session, form 추가

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Fcuser
+from .forms import LoginForm
 
 # Create your views here.
+
 
 def home(request):
     user_id = request.session.get('user')
@@ -14,29 +16,26 @@ def home(request):
 
     return HttpResponse('Home!')
 
-def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-        
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야 합니다.'
-        else:
-            fcuser = Fcuser.objects.get(username=username)
-            if check_password(password, fcuser.password):
-                # 비밀번호가 일치, 로그인 처리를 하면 됨
-                # 세션!
-                # 홈으로 re direct!
-                request.session['user'] = fcuser.id
-                return redirect('/')
-                pass
-            else:
-                res_data['error'] = '비밀번호가 틀렸습니다.'
 
-        return render(request, 'login.html', res_data)
+def logout(request):
+    if request.session.get('user'):
+        del(request.session['user'])
+
+    return redirect('/')
+
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # session code가 들어옴
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
+
 
 def register(request):
     if request.method == "GET":
@@ -55,9 +54,9 @@ def register(request):
             res_data['error'] = "비밀번호가 다릅니다!"
         else:
             fcuser = Fcuser(
-                username = username,
-                useremail = useremail,
-                password = make_password(password)    # 암호화
+                username=username,
+                useremail=useremail,
+                password=make_password(password)    # 암호화
             )
 
             fcuser.save()
