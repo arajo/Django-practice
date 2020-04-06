ef57648603806ee4f8b7906c8b62838392e68301
Ara jo <i_tomorrow_i@hanmail.net>
2020-04-06 15:08:06 +0900

    "pagination 추가"

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from django.http import Http404
 from fcuser.models import Fcuser
 from .models import Board
 from .forms import BoardForm
@@ -7,11 +9,18 @@ from .forms import BoardForm
 
 
 def board_detail(request, pk):
-    board = Board.objects.get(pk=pk)
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404('게시글을 찾을 수 없습니다.')
+
     return render(request, 'board_detail.html', {'board': board})
 
 
 def board_write(request):
+    if not request.session.get('user'):
+        return redirect('/fcuser/login/')
+
     if request.method == 'POST':
         form = BoardForm(request.POST)
         if form.is_valid():
@@ -33,5 +42,9 @@ def board_write(request):
 
 
 def board_list(request):
-    boards = Board.objects.all().order_by('-id')
+    all_boards = Board.objects.all().order_by('-id')
+    page = int(request.GET.get('p', 1))
+    paginator = Paginator(all_boards, 3)
+
+    boards = paginator.get_page(page)
     return render(request, 'board_list.html', {'boards': boards})
